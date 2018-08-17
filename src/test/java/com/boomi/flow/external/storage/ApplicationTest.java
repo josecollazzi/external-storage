438f6e74c6f9505fe936d55fb110a2a7e8dfc6f5
jose <jose.collazzi@manywho.com>
2018-08-17 17:01:20 +0100

    update

--- src/test/java/com/boomi/flow/external/storage/ApplicationTest.java
+++ src/test/java/com/boomi/flow/external/storage/ApplicationTest.java
@@ -5,28 +5,12 @@ import com.boomi.flow.external.storage.keys.KeyRepository;
 import com.boomi.flow.external.storage.keys.KeyRepositoryProvider;
 import com.boomi.flow.external.storage.states.StateRepository;
 import com.google.inject.AbstractModule;
-import com.google.inject.util.Providers;
 import com.manywho.sdk.services.servers.Servlet3Server;
 import org.jdbi.v3.core.Jdbi;
 
 import javax.inject.Singleton;
 
 public class ApplicationTest extends Servlet3Server {
-
-    public ApplicationTest() {
-
-        this.addModule(new AbstractModule() {
-            @Override
-            protected void configure() {
-                bind(Jdbi.class).toProvider(Providers.of(null));
-                bind(KeyRepository.class).toProvider(KeyRepositoryProvider.class).in(Singleton.class);
-                bind(StateRepository.class).toProvider(StateRepositoryProvider.class).in(Singleton.class);
-            }
-        });
-
-        this.setApplication(ApplicationTest.class);
-        this.start();
-    }
 
     public ApplicationTest(Jdbi jdbiSchema) {
         this.addModule(new AbstractModule() {
